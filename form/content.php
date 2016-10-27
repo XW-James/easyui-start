4dfb1f76039dca5a92b17ec9760d65c14bb78622
XW-James <asd@163.com>
2016-10-27 22:55:44 +0800

    easyui form datagrid-merge

---
+++ form/content.php
@@ -0,0 +1,8 @@
+<?php
+	echo $_POST['name'];
+	echo '|';
+	echo $_POST['email'];
+	echo '|';
+	echo $_POST['code'];
+
+?>
---
+++ form/js/index.js
@@ -0,0 +1,39 @@
+$(function () {
+
+	// $('#box').form({
+	// 	url : 'content.php',
+	// 	onSubmit : function (param) {
+	// 		param.code = '320902';
+	// 		return $(this).form('validate');
+	// 	},
+	// 	success : function (data) {
+	// 		alert(data);
+	// 		//var data = eval('(' + data + ')');
+	// 		//if (data.email) {
+	// 		//	alert(data.email);
+	// 		//}
+	// 	},
+	// });
+
+
+
+	// $('#box').form('load', {
+	// 		name : '蜡笔小新',
+	// 		email : 'xiaoxin@163.com',
+	// 	});
+
+	$('#box').form('load', 'load.json');
+
+})
+	
+
+
+
+
+
+
+
+
+
+
+
